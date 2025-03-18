725ab00eafb2cc00095beeffc91e3d672b8dfdf1
skoniks <skolya16@ya.ru>
2025-03-18 19:47:12 +0300

    Update index binary

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+#!/usr/bin/env node
+
 const { writeFileSync, mkdirSync } = require('fs');
 const { resolve, dirname } = require('path');
 
 try {
   const time = (Date.now() / 1000) | 0;
-  const path = resolve(process.argv[2]);
+  const args = process.argv.slice(2);
+  const path = resolve(args[0]);
   mkdirSync(dirname(path), { recursive: true });
   writeFileSync(path, `${time}`);
 } catch (error) {
